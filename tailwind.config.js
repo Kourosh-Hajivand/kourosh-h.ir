35e2e2adaf318990314b192873d293af6821c6a4
Kourosh-H <kourosh-h@Kourosh-H-MacBook-Pro.local>
2022-12-04 17:44:24 +0330

    Tailwind commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,5 +20,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"),
+  ],
 };
