b809c66d48ce98f63d587eba4da6d7834f6469bc
Kourosh-H <kourosh-h@Kourosh-H-MacBook-Pro.local>
2022-12-04 15:24:39 +0330

    second commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
